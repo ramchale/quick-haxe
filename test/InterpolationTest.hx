d508606c073c27f43fc054f44a20b6f6ae84a073
Richard McHale <richam@hotmail.co.uk>
2015-04-26 19:18:57 +0100

    Updating tests for incompatible hamcrest update

--- test/InterpolationTest.hx
+++ test/InterpolationTest.hx
@@ -17,13 +17,13 @@ class InterpolationTest
 	@Test
 	public function TestLinear()
 	{
-		MatcherAssert.assertThat('0 to 100 at t=0', Interpolation.linear(0, 100, 0), IsEqual.equalTo(0));
-		MatcherAssert.assertThat('0 to 100 at t=1', Interpolation.linear(0, 100, 1), IsEqual.equalTo(100));
-		MatcherAssert.assertThat('0 to 100 at t=0.5', Interpolation.linear(0, 100, 0.5), IsEqual.equalTo(50));
-		MatcherAssert.assertThat('-50 to 50 at t=0.2', Interpolation.linear(-50, 50, 0.2), IsEqual.equalTo(-30));
-		MatcherAssert.assertThat('-50 to 50 at t=0.8', Interpolation.linear(-50, 50, 0.8), IsEqual.equalTo(30));
-		MatcherAssert.assertThat('50 to -50 at t=0.3', Interpolation.linear(50, -50, 0.3), IsEqual.equalTo(20));
-		MatcherAssert.assertThat('50 to -50 at t=0.7', Interpolation.linear(50, -50, 0.7), IsEqual.equalTo(-20));
+		MatcherAssert.assertThat(Interpolation.linear(0, 100, 0), IsEqual.equalTo(0), '0 to 100 at t=0');
+		MatcherAssert.assertThat(Interpolation.linear(0, 100, 1), IsEqual.equalTo(100), '0 to 100 at t=1');
+		MatcherAssert.assertThat(Interpolation.linear(0, 100, 0.5), IsEqual.equalTo(50), '0 to 100 at t=0.5');
+		MatcherAssert.assertThat(Interpolation.linear(-50, 50, 0.2), IsEqual.equalTo(-30), '-50 to 50 at t=0.2');
+		MatcherAssert.assertThat(Interpolation.linear(-50, 50, 0.8), IsEqual.equalTo(30), '-50 to 50 at t=0.8');
+		MatcherAssert.assertThat(Interpolation.linear(50, -50, 0.3), IsEqual.equalTo(20), '50 to -50 at t=0.3');
+		MatcherAssert.assertThat(Interpolation.linear(50, -50, 0.7), IsEqual.equalTo(-20), '50 to -50 at t=0.7');
 	}
 	
 }
--- test/MathematicsTest.hx
+++ test/MathematicsTest.hx
@@ -16,29 +16,29 @@ class MathematicsTest
 	@Test
 	public function TestDegreesToRadians()
 	{
-		MatcherAssert.assertThat('0 degrees', Mathematics.degreesToRadians(0), IsEqual.equalTo(0));
-		MatcherAssert.assertThat('90 degrees', Mathematics.degreesToRadians(90), IsEqual.equalTo(Math.PI * 0.5));
-		MatcherAssert.assertThat('180 degrees', Mathematics.degreesToRadians(180), IsEqual.equalTo(Math.PI));
-		MatcherAssert.assertThat('270 degrees', Mathematics.degreesToRadians(270), IsEqual.equalTo(Math.PI * 1.5));
-		MatcherAssert.assertThat('360 degrees', Mathematics.degreesToRadians(360), IsEqual.equalTo(Math.PI * 2.0));
+		MatcherAssert.assertThat(Mathematics.degreesToRadians(0), IsEqual.equalTo(0), '0 degrees');
+		MatcherAssert.assertThat(Mathematics.degreesToRadians(90), IsEqual.equalTo(Math.PI * 0.5), '90 degrees');
+		MatcherAssert.assertThat(Mathematics.degreesToRadians(180), IsEqual.equalTo(Math.PI), '180 degrees');
+		MatcherAssert.assertThat(Mathematics.degreesToRadians(270), IsEqual.equalTo(Math.PI * 1.5), '270 degrees');
+		MatcherAssert.assertThat(Mathematics.degreesToRadians(360), IsEqual.equalTo(Math.PI * 2.0), '360 degrees');
 	}
 	
 	@Test
 	public function TestRadiansToDegrees()
 	{
-		MatcherAssert.assertThat('0 radians', Mathematics.radiansToDegrees(0), IsEqual.equalTo(0));
-		MatcherAssert.assertThat('PI / 2 radians', Mathematics.radiansToDegrees(Math.PI * 0.5), IsEqual.equalTo(90));
-		MatcherAssert.assertThat('PI radians', Mathematics.radiansToDegrees(Math.PI), IsEqual.equalTo(180));
-		MatcherAssert.assertThat('3 * PI / 2 radians', Mathematics.radiansToDegrees(Math.PI * 1.5), IsEqual.equalTo(270));
-		MatcherAssert.assertThat('2 * PI radians', Mathematics.radiansToDegrees(Math.PI * 2.0), IsEqual.equalTo(360));
+		MatcherAssert.assertThat(Mathematics.radiansToDegrees(0), IsEqual.equalTo(0), '0 radians');
+		MatcherAssert.assertThat(Mathematics.radiansToDegrees(Math.PI * 0.5), IsEqual.equalTo(90), 'PI / 2 radians');
+		MatcherAssert.assertThat(Mathematics.radiansToDegrees(Math.PI), IsEqual.equalTo(180), 'PI radians');
+		MatcherAssert.assertThat(Mathematics.radiansToDegrees(Math.PI * 1.5), IsEqual.equalTo(270), '3 * PI / 2 radians');
+		MatcherAssert.assertThat(Mathematics.radiansToDegrees(Math.PI * 2.0), IsEqual.equalTo(360), '2 * PI radians');
 	}
 	
 	@Test
 	public function ModularDifference()
 	{
-		MatcherAssert.assertThat('0 - 0 (mod 4)', Mathematics.modularDifference(0, 0, 4), IsEqual.equalTo(0));
-		MatcherAssert.assertThat('4 - 0 (mod 4)', Mathematics.modularDifference(4, 0, 4), IsEqual.equalTo(0));
-		MatcherAssert.assertThat('5 - 3 (mod 4)', Mathematics.modularDifference(5, 3, 4), IsEqual.equalTo(2));
-		MatcherAssert.assertThat('2 - 3 (mod 4)', Mathematics.modularDifference(2, 3, 4), IsEqual.equalTo(3));
+		MatcherAssert.assertThat(Mathematics.modularDifference(0, 0, 4), IsEqual.equalTo(0), '0 - 0 (mod 4)');
+		MatcherAssert.assertThat(Mathematics.modularDifference(4, 0, 4), IsEqual.equalTo(0), '4 - 0 (mod 4)');
+		MatcherAssert.assertThat(Mathematics.modularDifference(5, 3, 4), IsEqual.equalTo(2), '5 - 3 (mod 4)');
+		MatcherAssert.assertThat(Mathematics.modularDifference(2, 3, 4), IsEqual.equalTo(3), '2 - 3 (mod 4)');
 	}
 }
