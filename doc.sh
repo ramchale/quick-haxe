3b9ca8deff5ccc1ac603e608099a60ec670c2ab4
Richard McHale <legionsofbob@gmail.com>
2016-01-27 16:16:33 +0000

    Working except the Drawing class

---
+++ doc.sh
@@ -0,0 +1,3 @@
+cd docs
+haxe doc.hxml
+haxelib run dox -i doc_gen.xml -in quick_haxe
--- quick_haxe/Drawing.hx
+++ quick_haxe/Drawing.hx
@@ -1,28 +1,34 @@
 package quick_haxe;
 
-import openfl.display.GraphicsPathCommand;
-import openfl.display.Sprite;
-
+// TODO - Fix the dependancy issues with this class
+// (Maybe move it to it's own library anyway)
 class Drawing
 {
-	public static function DrawPath(sprite:Sprite, coordinates:Array<Float>, colour:Int = 0xFFFFFF, thickness:Float = 1) : Void
-	{
-		var commands:Array<Int> = new Array<Int>();
-		
-		for (i in 0...cast(coordinates.length / 2, Int))
-		{
-			if (commands.length > 0)
-			{
-				commands.push(GraphicsPathCommand.LINE_TO);
-			}
-			else
-			{
-				commands.push(GraphicsPathCommand.MOVE_TO);
-			}
-		}
-		
-		sprite.graphics.lineStyle(thickness, colour);
-		sprite.graphics.drawPath(commands, coordinates);
-	}
-	
 }
+//
+// import openfl.display.GraphicsPathCommand;
+// import openfl.display.Sprite;
+//
+// class Drawing
+// {
+// 	public static function DrawPath(sprite:Sprite, coordinates:Array<Float>, colour:Int = 0xFFFFFF, thickness:Float = 1) : Void
+// 	{
+// 		var commands:Array<Int> = new Array<Int>();
+//
+// 		for (i in 0...cast(coordinates.length / 2, Int))
+// 		{
+// 			if (commands.length > 0)
+// 			{
+// 				commands.push(GraphicsPathCommand.LINE_TO);
+// 			}
+// 			else
+// 			{
+// 				commands.push(GraphicsPathCommand.MOVE_TO);
+// 			}
+// 		}
+//
+// 		sprite.graphics.lineStyle(thickness, colour);
+// 		sprite.graphics.drawPath(commands, coordinates);
+// 	}
+//
+// }
